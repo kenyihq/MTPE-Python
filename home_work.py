378355b3d2678c967d6bd06151e556f480b7fb2c
Kenyi Hancco <kenyi@gmail.com>
2022-10-24 17:56:16 -0500

    Added homework, second week

---
+++ home_work.py
@@ -0,0 +1,60 @@
+"""
+Ejercicio 1: Hacer un programa que pida al usuario 5 nombres. Crear una lista con los 5 nombres. Despues hacer que muestre una frase los pronombres que empiezan con la letra A
+"""
+
+
+"""
+Ejercicio 2: Haga un programa en Python que le pida al usuario tantos enteros como quiera, luego cree dos listas, una con la lista de números propuestos y la otra con
+el número de ocurrencias. Por ejemplo, si el usuario ingresa 4,4,8,4,9,7,7, la primera lista
+debe ser [4,8,9,7] y el segundo [3,1,1,2] 
+"""
+
+""" *
+Ejercicio 3: Dada la matriz, [[1,2,3],[4,5,6],[7,8,9]], calcule el promedio de la diagonal principal. Hint: Los 3 elementos de la diagonal son 1,5,9
+"""
+
+""" *
+Ejercicio 4: Dada la siguiente lista ["Hola", "Amigos", "Hoy", True] , escriba un programa que pida al usuario una palabra, dicha palabra debe ser agregada al final y al inicio de la lista.
+"""
+
+
+""" *
+Ejercicio 4
+Dada una lista de números entero    s [15,20,50,80,40,60], escriba un programa que dado un dato por el usuario, este sea eliminado de la lista. Tome en cuenta que el usuario ingresará datos que se encuentran dentro de la lista
+Ejemplo:
+Ingrese el dato a eliminar: 60
+Salida: [15,20,50,80,40]
+"""
+
+"""
+Ejercicio 5 *
+Dada una tupla de números (1,3,5,2,7,5,5,8,4,8,4,8,4), escriba un programa que dado un elemento por el usuario, imprima el número de veces que se encuentra en la tupla.
+Ejemplo:
+Ingrese el elemento a contar: 4
+Salida: 3
+"""
+
+"""
+Ejercicio 6 *
+Dado el diccionario que almacena la talla de algunas personas {'Marcelo': 1.80, 'José':1.50, 'Oscar':1.70, 'Jorge': 1.40}, escriba un programa que dado un nombre ingresado por el usuario imprime la talla.
+Ejemplo:
+Ingrese un nombre: Marcelo
+Salida: 1.80
+"""
+
+"""
+Ejercicio 7
+Dado el diccionario que almacena la talla de algunas personas {'Marcelo': 1.80, 'José':1.50, 'Oscar':1.70, 'Jorge': 1.40}, escriba un programa que dada una talla por el usuario imprima el nombre.
+Ejemplo:
+Ingrese una talla: 1.80
+Salida: Marcelo
+"""
+
+"""
+Ejercicio 8 *
+Guarde los datos de una persona (nombre,apellido,edad) en un diccionario, luego imprimalo en el siguiente formato. "Hola mi nombre es [nombre] [apellido], y tengo [edad] años.
+"""
+
+
+"""
+"""
